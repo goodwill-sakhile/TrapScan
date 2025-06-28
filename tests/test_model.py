cd6437bfa2adbfca019cec8c96d05a7bd043a44a
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-29 00:36:06 +0200

    test_model.py Script

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,4 @@
+
 # tests/test_model.py
 
 import pytest
@@ -8,6 +9,7 @@ from api.predict import classify_image
 def test_model_prediction_on_dummy_image():
     """
     Test CNN classification on a dummy white image.
+    
     """
     dummy_image = Image.fromarray(np.ones((128, 128, 3), dtype=np.uint8) * 255)
     label, confidence = classify_image(dummy_image)
